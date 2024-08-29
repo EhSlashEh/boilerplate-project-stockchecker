d7eafdb8a03c468822f70d005d24b98b6beb1c79
EhSlashEh <anthonyalanb@hotmail.com>
2024-08-29 20:34:59 -0300

    First save

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,8 @@
 
 module.exports = function (app) {
 
+  // https://stock-price-checker-proxy.freecodecamp.rocks/v1/stock/[symbol]/quote
+
   app.route('/api/stock-prices')
     .get(function (req, res){
       
